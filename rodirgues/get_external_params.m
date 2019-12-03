51eeed4459028aa4f5de45f6ff4fe3056a50172b
Hengchang Lu <luhengchang228@gmail.com>
2019-12-03 13:41:24 +0800

    chore: update get_external_params.m

--- rodirgues/get_external_params.m
+++ rodirgues/get_external_params.m
@@ -1,17 +1,20 @@
+% 求解外参
+% external_matrix 3 * 4 [R, t]
+
 function [external_matrix]  = get_external_params()
 
+% load data from the Calib_Results.mat
 calib_result = load('Calib_Results.mat');
 
 length = 7;
 external_matrix = zeros(3, 4, 7);
 
 for i = 1:length
-    % load data from the Calib_Results.mat
     omc = calib_result.(strcat('omc_', num2str(i)));
     t = calib_result.(strcat('Tc_', num2str(i)));
     % rodirgues transform vec to matrix 
-    rotVecToMat = rotationVectorToMatrix(omc);
-    out = [rotVecToMat, t];
+    R = rotationVectorToMatrix(omc);
+    out = [R, t];
     external_matrix(:, :, i) = out;
 end
 end
