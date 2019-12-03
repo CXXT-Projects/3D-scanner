f8cc0e2f5715bfaee6a511679da61c50d2ea232f
Hengchang Lu <luhengchang228@gmail.com>
2019-12-03 13:39:53 +0800

    feat: create find_result.mat

--- find/find_u_v.m
+++ find/find_u_v.m
@@ -1,26 +1,30 @@
 % find the param u and v
 % input_img_num 输入图片数量
-% input_file_name 
-% output_file_name 
+% input_file_name 输入图片的文件名
+% output_file_name 输出文件名  类型是.mat
 
 function [s]  = find_u_v( input_img_num, input_file_name, output_file_name )
 
 for i = 1:input_img_num
     data = imread(strcat(input_file_name, num2str(i), '.bmp'));
-
+    
+%     find() 返回向量或者矩阵中不为0的元素的位置索引
     [row, col] = find(data);
-    fid=fopen(strcat(output_file_name, num2str(i), '.txt'), 'wt');
+
     row_length = length(row(:));
     col_length = length(col(:));
+    
+    % catch error
     if row_length ~= col_length
         fprintf('Error: the length of the row is not equal to the length of the col');
         fprintf('\n');
         continue
     end
-    
-    for m = 1 : row_length
-        fprintf(fid, '%d %d\n', row(m), col(m));
-        
-    end
+%   分别生成不同的field
+    field_name = strcat('find_', num2str(i));
+    data = [col, row];
+    eval([field_name, '=', mat2str(data), ';']);
+%     保存为.mat文件
+    save(output_file_name, field_name, '-append');
     
 end
--- rodirgues/get_external_params.m
+++ rodirgues/get_external_params.m
@@ -1,14 +1,14 @@
 function [external_matrix]  = get_external_params()
 
-mat = load('Calib_Results.mat');
+calib_result = load('Calib_Results.mat');
 
 length = 7;
 external_matrix = zeros(3, 4, 7);
 
 for i = 1:length
     % load data from the Calib_Results.mat
-    omc = mat.(strcat('omc_', num2str(i)));
-    t = mat.(strcat('Tc_', num2str(i)));
+    omc = calib_result.(strcat('omc_', num2str(i)));
+    t = calib_result.(strcat('Tc_', num2str(i)));
     % rodirgues transform vec to matrix 
     rotVecToMat = rotationVectorToMatrix(omc);
     out = [rotVecToMat, t];
