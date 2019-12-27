b67ea47dbb4570a5276c5648beb9cfac15daf2fd
Hengchang Lu <luhengchang228@gmail.com>
2019-12-27 11:06:51 +0800

    feat: add circle_center

---
+++ AxisCalibration/scripts/calc_circle_center.m
@@ -0,0 +1,28 @@
+function [] = calc_circle_center(height_num, c_c_same_height_data_file_path, output_file_name)
+
+c_c_same_height_data = load(c_c_same_height_data_file_path);
+
+for i = 1: height_num
+    
+    for j = 1 : 2
+        field_name = strcat('circle_center', num2str(i), '_', num2str(j));
+        data = c_c_same_height_data.(strcat('c_c_same_height', num2str(i), '_', num2str(j)));
+        x = data(:, 1);
+        y = data(:, 2);
+        z = data(:, 3);
+        ymean = mean(y);
+        [k, resnorm] = fitting_circle_center_2d(x, z);
+        result = [k(1), ymean, k(2)];
+        
+        eval([field_name, '=', mat2str(result), ';']);
+        if i == 1 && j == 1
+            save(output_file_name, field_name);
+        else
+            save(output_file_name, field_name, '-append');
+        end
+        
+    end
+    
+end
+
+end
